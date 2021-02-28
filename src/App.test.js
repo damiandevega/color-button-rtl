af3b4bcf52a0ddf31565d1c02f4a4d8c8d0a3126
Damian de Vega <damiandevega@gmail.com>
2021-02-27 22:24:09 -0500

    Adding unit test for replaceCamelWithSpaces function

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App, { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color', () => {
   render(<App />)
@@ -75,3 +75,17 @@ test('button is changed to blue then gray when disabled and back to blue when en
   fireEvent.click(checkbox)
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
 });
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  })
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })
+})
