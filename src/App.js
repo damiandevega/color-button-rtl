105c59f64bccf83f06b0c0309c79eed2d641149e
Damian de Vega <damiandevega@gmail.com>
2021-02-27 20:19:08 -0500

    Adding another test for checkbox feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [disabled, setDisabled] = useState(false)
   const [buttonColor, setButtonColor] = useState('red')
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
   return (
@@ -9,9 +10,16 @@ function App() {
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={(e) => setButtonColor(newButtonColor)}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        id="enable-button-check"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onClick={(e) => setDisabled(e.target.checked)} />
     </div>
   );
 }
